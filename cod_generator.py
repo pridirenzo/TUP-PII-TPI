98f88aa1cced8aaf22f9e43b43483eb8e6d64e98
Mariano Castelli <marianocastelli12@gmail.com>
2023-10-23 12:16:17 -0300

    ver_cursos3

--- cod_generator.py
+++ cod_generator.py
@@ -4,5 +4,5 @@ import string
 # get random password pf length 8 with letters, digits, and symbols
 def generar():
     characters = string.ascii_letters + string.digits
-    cod = ''.join(random.choice(characters) for i in range(8))
+    cod = ''.join(random.choice(characters) for i in range(4))
     return cod
